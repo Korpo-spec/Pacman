c90dc9de0456bcb0319ccce808714b3cef2e9f20
Korpo-spec <casper22x@gmail.com>
2021-09-27 19:49:21 +0200

    Init commit

---
+++ Pacman/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using SFML.System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Pacman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (var window = new RenderWindow(new VideoMode(828, 900), "Platformer")) 
+            {
+                window.Closed += (o, e) => window.Close();
+
+                Clock clock = new Clock();
+                while (window.IsOpen)
+                {
+                    window.DispatchEvents();
+
+                    float deltaTime = clock.Restart().AsSeconds();
+                    
+                    window.Clear();
+                    // TODO:  Drawing
+                    
+                    window.Display();
+                }
+            }
+        }
+    }
+}
